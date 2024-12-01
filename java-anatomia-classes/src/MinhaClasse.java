f43a8c6e0e198ebcff2a535fe3605fce3e134e07
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-12-01 20:10:12 -0300

    Apenas validando variaveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -10,5 +10,17 @@ public class MinhaClasse {
         ano = 2024;
 
         System.out.println("Minha classe!" + " " + BR + " " + ano);
+
+        // Algumas variaveis invalidas
+        // int numero&um = 1;
+        // int 1numero = 1;
+        // int numero um = 1;
+        // int long = 1;
+
+        // correção das variaveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1;
+        int longo = 1;
     }
 }
